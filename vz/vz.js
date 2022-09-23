434c947e4dcd419e1b2384e2a483bdc566c6893e
Vadim <vadim.beglov@gmail.com>
2022-09-22 21:24:09 -0400

    vz

---
+++ vz/vz.js
@@ -0,0 +1,8 @@
+$(document).ready(function (){
+    $('#content').append('<div class="header"><img src="https://etozoc.github.io/vz/vz_logo.svg"></div>').append('<div class="main-body">' +
+        '<p>На сайте ведутся технические работы</p>' +
+        '<p>Все заказы обрабатываются в обычном режиме</p>' +
+        '<p>Наш телефон: <a href="tel:+78126793209">+7 (812) 679-32-09</a></p>' +
+        '<p>Наш адрес: Санкт-Петербург, Смоленская ул. , 31</p>' +
+        '</div>');
+})
